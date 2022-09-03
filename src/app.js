f0a5ee4dc031c432bbf5dfc0eaed76ddc894227a
Tanya <ovf.tanya@gmail.com>
2022-09-03 12:18:25 +0200

    refactoring

--- src/app.js
+++ src/app.js
@@ -1,137 +1,160 @@
 const apiKey = "d743afc6b8e9d42ff2b1cf896acc3bf6";
+const weather = {
+  tempFormat: "C",
+  current: {},
+  daily: [],
+};
 
-const now = new Date();
-const dateTime = document.querySelector("#date-time");
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-dateTime.innerHTML = `${day}, ${hours}:${minutes}`;
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
-}
-
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        ` <div class="col-2">
-              <div class="weather-forecast-date">${formatDay(
-                forecastDay.dt
-              )}</div>
-              <img
-                src="http://openweathermap.org/img/wn/${
-                  forecastDay.weather[0].icon
-                }@2x.png"
-                alt=""
-                width="42"
-              />
-              <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max"> ${Math.round(
-                  forecastDay.temp.max
-                )}° </span>
-                <span class="weather-forecast-temperature-min"> ${Math.round(
-                  forecastDay.temp.min
-                )}° </span>
-              </div>
-            </div>`;
-    }
+function fmtDayTime(date = new Date()) {
+  return date.toLocaleDateString("en-US", {
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
   });
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "d743afc6b8e9d42ff2b1cf896acc3bf6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+function fmtShortDay(date = new Date()) {
+  return date.toLocaleDateString("en-US", { weekday: "short" });
 }
 
-const btnSearch = document.querySelector("#button-search");
-const btnCurrent = document.querySelector("#button-current");
-const linkUnitC = document.querySelector("#celcius-link");
-const linkUnitF = document.querySelector("#fahrenheit-link");
+function convertToF(celsius) {
+  return Math.round((celsius * 9) / 5 + 32);
+}
 
-const currentWeather = { tempC: 0, tempF: 0, cityName: "" };
+function prepareCurrentWeather(data) {
+  return {
+    cityName: data.name,
+    coord: data.coord,
+    humidity: data.main.humidity,
+    icon: data.weather[0].icon,
+    temp: { C: Math.round(data.main.temp), F: convertToF(data.main.temp) },
+    windSpeed: Math.round(data.wind.speed),
+    dayTime: fmtDayTime(new Date(data.dt * 1000)),
+  };
+}
 
-function showTemperature(response) {
+function prepareDailyForecast(dailyForecast) {
+  return dailyForecast.slice(0, 6).map((day) => ({
+    day: fmtShortDay(new Date(day.dt * 1000)),
+    icon: day.weather[0].icon,
+    maxTemp: { C: Math.round(day.temp.max), F: convertToF(day.temp.max) },
+    minTemp: { C: Math.round(day.temp.min), F: convertToF(day.temp.min) },
+  }));
+}
+
+function displayForcast() {
+  const cityNameEl = document.querySelector("#cityName");
+  const forecastEl = document.querySelector("#forecast");
+  const humidityEl = document.querySelector("#humidity");
+  const iconEl = document.querySelector("#icon");
   const tempEl = document.querySelector("#temp");
-  const cityNameH2 = document.querySelector("#cityName");
-  const iconElement = document.querySelector("#icon");
+  const windEl = document.querySelector("#wind");
+  const dateEl = document.querySelector("#date-time");
 
-  const temp = Math.round(response.data.main.temp);
-  const cityName = response.data.name;
+  const tempFormat = weather.tempFormat;
+  const iconUrl = `http://openweathermap.org/img/wn/${weather.current.icon}@2x.png`;
 
-  document.querySelector("#humidity").innerText = response.data.main.humidity;
-  document.querySelector("#wind").innerText = Math.round(
-    response.data.wind.speed
-  );
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  currentWeather.tempC = temp;
-  currentWeather.tempF = Math.round((temp * 9) / 5 + 32);
-  currentWeather.cityName = cityName;
+  dateEl.innerHTML = weather.current.dayTime;
+  humidityEl.innerText = weather.current.humidity;
+  windEl.innerText = weather.current.windSpeed;
+  cityNameEl.innerText = weather.current.cityName;
+  tempEl.innerText = weather.current.temp[tempFormat];
+  iconEl.setAttribute("src", iconUrl);
 
-  cityNameH2.innerText = cityName;
-  tempEl.innerText = temp;
+  const dailyHTML = weather.daily
+    .map((day) => {
+      return `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day.day}</div>
+          <img src="http://openweathermap.org/img/wn/${day.icon}@2x.png" alt="" width="42"/>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> ${day.maxTemp[tempFormat]}° </span>
+            <span class="weather-forecast-temperature-min"> ${day.minTemp[tempFormat]}° </span>
+          </div>
+        </div>`;
+    })
+    .join("");
 
-  getForecast(response.data.coord);
+  forecastEl.innerHTML = `<div class="row">${dailyHTML}</div>`;
 }
 
-btnSearch.addEventListener("click", (event) => {
+function search(q = "", lat = 0, lon = 0) {
+  getCurrentWeather(q, lat, lon)
+    .then((currentWeather) => {
+      weather.current = currentWeather;
+
+      return getForecast(weather.current.coord.lat, weather.current.coord.lon);
+    })
+    .then((dailyForecast) => {
+      weather.daily = dailyForecast;
+    })
+    .then(() => {
+      displayForcast();
+    });
+}
+
+function getForecast(lat, lon) {
+  return axios
+    .get(`https://api.openweathermap.org/data/2.5/onecall`, {
+      params: {
+        units: "metric",
+        appid: apiKey,
+        lon,
+        lat,
+        exclude: "current,minutely,hourly,alerts",
+      },
+    })
+    .then(({ data }) => data.daily)
+    .then(prepareDailyForecast);
+}
+
+function getCurrentWeather(q = "", lat = 0, lon = 0) {
+  return axios
+    .get(`https://api.openweathermap.org/data/2.5/weather`, {
+      params: { units: "metric", appid: apiKey, q, lon, lat },
+    })
+    .then(({ data }) => data)
+    .then(prepareCurrentWeather);
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  const cityNameEl = document.querySelector("#city-name");
-  const cityName = cityNameEl.value;
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`;
+  const cityName = document.querySelector("#city-name").value;
 
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
-});
+  search(cityName);
+}
 
-btnCurrent.addEventListener("click", (event) => {
+function handleGeolocation(event) {
   event.preventDefault();
 
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
 
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+    document.querySelector("#city-name").value = "";
+
+    search("", latitude, longitude);
   });
+}
+
+document.querySelector("#search-form").addEventListener("submit", handleSubmit);
+document.querySelector("#button-geo").addEventListener("click", handleGeolocation);
+
+document.querySelector("#celcius-link").addEventListener("click", (event) => {
+  event.preventDefault();
+
+  weather.tempFormat = "C";
+
+  displayForcast();
 });
 
-linkUnitC.addEventListener("click", (event) => {
+document.querySelector("#fahrenheit-link").addEventListener("click", (event) => {
   event.preventDefault();
-  document.querySelector("#temp").innerText = currentWeather.tempC;
+
+  weather.tempFormat = "F";
+
+  displayForcast();
 });
 
-linkUnitF.addEventListener("click", (event) => {
-  event.preventDefault();
-  document.querySelector("#temp").innerText = currentWeather.tempF;
-});
+search("Cherkasy");
